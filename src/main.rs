15a4e446c7dce58f798bbd68372da8c10735d33f
dumpstr <lithenator@gmail.com>
2021-05-20 15:05:42 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("the super secret number is: {}", secret_number);
+    loop {
+        println!("input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("u did it!! woohoo!");
+                break;
+            }
+        }
+    }
+}
